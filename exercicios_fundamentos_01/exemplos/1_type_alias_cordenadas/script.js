29d1b350339aeb7b4d9e2ba4c023189bf5bd5aac
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-08 01:27:32 -0400

    segundo exemplo de type alias

--- exercicios_fundamentos_01/exemplos/1_type_alias_cordenadas/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const calcularDistancia = (pontoA, pontoB) => {
-    return Math.sqrt(Math.pow(pontoB.latitude - pontoA.longitude, 2) +
-        Math.pow(pontoB.longitude - pontoA.latitude, 2));
-};
-// criando os dois pontos 
-const pontoA = { latitude: 10, longitude: 65 };
-const pontoB = { latitude: 100, longitude: 165 };
-console.log(calcularDistancia(pontoA, pontoB));
