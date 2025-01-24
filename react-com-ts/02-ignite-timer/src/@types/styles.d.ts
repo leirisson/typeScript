dd6baf34a5fde8d6c604103da13f9e79136f6cbc
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-24 08:42:06 -0400

    subir

--- react-com-ts/02-ignite-timer/src/@types/styles.d.ts
+++ react-com-ts/02-ignite-timer/src/@types/styles.d.ts
@@ -6,6 +6,7 @@ import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = typeof defaultTheme;
 
+
 declare module 'styled-components'{
     export interface DefaultTheme extends ThemeType {}
 };
